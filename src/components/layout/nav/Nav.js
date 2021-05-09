f2d8f18b323200ff0cebd9a5d58d6174ee9a61b9
Florent GALLOU <florentgallou@gmail.com>
2021-05-09 23:21:20 +0200

    social links added

--- src/components/layout/nav/Nav.js
+++ src/components/layout/nav/Nav.js
@@ -9,7 +9,7 @@ function Nav(){
                     <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
                 </button>
                 <div className="row collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center align-items-center">
                         <li className="nav-item">
                             <Link className="nav-link lightText fw-bold text-center" to="/home" aria-label="Afficher mes projets">Projets</Link>
                         </li>
@@ -21,6 +21,15 @@ function Nav(){
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link lightText fw-bold text-center" to="/contact" aria-label="Afficher mes Technologies">Contact</Link>
+                        </li>
+                        <li className="nav-item p-2">
+                            <a className="lightText" href="https://github.com/FlorentGallou-Dev" aria-label="Mon github" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
+                        </li>
+                        <li className="nav-item p-2">
+                            <a className="lightText" href="https://linkedin.com/in/florent-gallou" aria-label="Mon twitter" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
+                        </li>
+                        <li className="nav-item p-2">
+                            <a className="lightText" href="https://twitter.com/gallou_florent" aria-label="Mon twitter" target="_blank" rel="noreferrer"><i class="fab fa-twitter"></i></a>
                         </li>
                     </ul>
                 </div>
